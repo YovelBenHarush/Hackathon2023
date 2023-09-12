8d176aaca206b791bfed9bdd86911dbc28bbcc5d
Yovel Mordechai Ben Harosh <ybenharosh@microsoft.com>
2023-09-12 18:41:07 +0300

    Grpc client

--- Hackathon2023/Program.cs
+++ Hackathon2023/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using Hackathon2023;
+
+Console.WriteLine("Starting...");
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:49157");
+var client = new VAGrpc.VAGrpcClient(channel);
+var reply = await client.ExecuteQueryAsync(
+                  new QueryRequest { InstanceName = "SQLEXPRESS", DbName = "master", Query = "SELECT * FROM sys.tables" });
+Console.WriteLine($"returnCode: {reply.ReturnCode}, results: {reply.QueryResults}");
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
