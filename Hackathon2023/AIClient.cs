c3b00bf9ba8a3e510db92f9cc5f42c3d401527d0
Yovel Mordechai Ben Harosh <ybenharosh@microsoft.com>
2023-09-13 15:16:00 +0300

    Add skelaton + AIClient

---
+++ Hackathon2023/AIClient.cs
@@ -0,0 +1,131 @@
+﻿using AI.Dev.OpenAI.GPT;
+using Azure;
+using Azure.AI.OpenAI;
+
+namespace Microsoft.DataSec.DspmForDatabases.Tools.HackathonScanEngineRunner
+{
+    public class AIClient
+    {
+        private const int TokenLimit = 16384;
+        private const int MaxResponseTokens = 1000;
+
+        private readonly string _key = "<>";
+        private readonly string _endpoint = "https://emeaopenai.azure-api.net/";
+        private readonly string _deployment = "gpt-35-turbo-16k";
+        private readonly OpenAIClient _client;
+        private readonly ChatCompletionsOptions _defaultOptions;
+        private readonly CompletionsOptions _defaultCompletionsOptions;
+
+        private int _tokensUsed = 0;
+
+        public AIClient(IList<ChatMessage> defaultInstructions)
+        {
+            _client = new OpenAIClient(new Uri(_endpoint), new AzureKeyCredential(_key));
+            _defaultOptions = GetDefaultChatCompletionsOptions(defaultInstructions);
+            _defaultCompletionsOptions = GetDefaultCompletionsOptions(defaultInstructions);
+        }
+
+        public async Task<ChatCompletions> GetChatCompletionsAsync(List<(ChatRole Role, string Message)> messages, CancellationToken cancellationToken)
+        {
+            try
+            {
+                for (int i = messages.Count - 1; i >= 0; i--)
+                {
+                    var msg = messages[i];
+                    _tokensUsed += GPT3Tokenizer.Encode(msg.Message).Count();
+
+                    if (_tokensUsed > TokenLimit)
+                    {
+                        messages.RemoveRange(0, i);
+                        break;
+                    }
+                }
+
+                var chatCompletionsOptions = _defaultOptions;
+                var chatMessages = messages.Select(m => new ChatMessage(m.Role, m.Message)).ToList();
+                chatMessages.ForEach(chatCompletionsOptions.Messages.Add);
+
+                Response<ChatCompletions> response = await _client.GetChatCompletionsAsync(
+                deploymentOrModelName: _deployment,
+                chatCompletionsOptions,
+                cancellationToken);
+
+                chatMessages.ForEach(m => chatCompletionsOptions.Messages.Remove(m));
+
+                return response.Value;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed calculating classifications", ex);
+                return null;
+            }
+        }
+
+        public async Task<Completions> GetCompletionsAsync(List<string> messages, CancellationToken cancellationToken)
+        {
+            try
+            {
+                for (int i = messages.Count - 1; i >= 0; i--)
+                {
+                    var msg = messages[i];
+                    _tokensUsed += GPT3Tokenizer.Encode(msg).Count();
+
+                    if (_tokensUsed > TokenLimit)
+                    {
+                        messages.RemoveRange(0, i);
+                        break;
+                    }
+                }
+
+                var completionsOptions = _defaultCompletionsOptions;
+                messages.ForEach(completionsOptions.Prompts.Add);
+
+                Response<Completions> response = await _client.GetCompletionsAsync(
+                deploymentOrModelName: _deployment,
+                completionsOptions,
+                cancellationToken);
+
+                messages.ForEach(m => completionsOptions.Prompts.Remove(m));
+
+                return response.Value;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed calculating classifications", ex);
+                return null;
+            }
+        }
+
+        private ChatCompletionsOptions GetDefaultChatCompletionsOptions(IList<ChatMessage> systemMessages)
+        {
+            foreach (var msg in systemMessages)
+            {
+                _tokensUsed += GPT3Tokenizer.Encode(msg.Content).Count();
+            }
+
+            _tokensUsed += 3 * MaxResponseTokens;
+
+            return new ChatCompletionsOptions(systemMessages)
+            {
+                Temperature = 0.0f,
+                MaxTokens = MaxResponseTokens,
+            };
+        }
+
+        private CompletionsOptions GetDefaultCompletionsOptions(IList<ChatMessage> systemMessages)
+        {
+            foreach (var msg in systemMessages)
+            {
+                _tokensUsed += GPT3Tokenizer.Encode(msg.Content).Count();
+            }
+
+            _tokensUsed += 3 * MaxResponseTokens;
+
+            return new CompletionsOptions(systemMessages.Select(m => m.Content))
+            {
+                Temperature = 0.0f,
+                MaxTokens = MaxResponseTokens,
+            };
+        }
+    }
+}
---
+++ Hackathon2023/AskDBRunner.cs
@@ -0,0 +1,119 @@
+﻿using Azure.AI.OpenAI;
+using Grpc.Net.Client;
+using Microsoft.DataSec.DspmForDatabases.Tools.HackathonScanEngineRunner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon2023
+{
+    public class AskDBRunner
+    {
+        private const string HostName = "localhost";
+        private const string Port = "49157";
+
+        private readonly string _instanceName;
+        private readonly string _dbName;
+
+        private readonly VAGrpc.VAGrpcClient _grpcClient;
+        private readonly AIClient _aiClient;
+
+        public AskDBRunner(string instanceName, string dbName)
+        {
+            var channel = GrpcChannel.ForAddress($"http://{HostName}:{Port}");
+            _grpcClient = new VAGrpc.VAGrpcClient(channel);
+            _aiClient = new AIClient(GetDefaultChatInstructions());
+            _instanceName = instanceName;
+            _dbName = dbName;
+        }
+
+        public async Task RunAsync()
+        {
+            bool isSuccess = false;
+            string? userRequest = null;
+
+            Console.WriteLine("Starting...");
+            do
+            {
+                do
+                {
+                    Console.WriteLine("What would you like to know about your database?");
+
+                    userRequest = Console.ReadLine();
+                }
+                while (userRequest == null);
+
+                var tableNames = await ExtractTableNames(userRequest);
+
+                var schemas = await GetSchemasForTables(tableNames);
+
+                var query = await BuildQuery(schemas, userRequest);
+
+                if (!ValidateQuery(query))
+                {
+                    Console.WriteLine("Invalid query");
+                }
+                else
+                {
+                    var result = await RunQuery(query);
+
+                    Console.WriteLine("Query result:");
+                    Console.WriteLine(result);
+                }
+            }
+            while (!isSuccess);
+        }
+
+        private async Task<List<string>> ExtractTableNames(string userRequest)
+        {
+            return new List<string>();
+        }
+
+        private async Task<List<string>> GetSchemasForTables(List<string> tableNames)
+        {
+            List<string> schemas = new List<string>();
+
+            foreach (var tableName in tableNames)
+            {
+                var reply = await _grpcClient.ExecuteQueryAsync(
+                                   new QueryRequest { InstanceName = _instanceName, DbName = _dbName, Query = "SELECT * FROM sys.tables" });
+
+                schemas.Add(reply.QueryResults);
+            }
+
+            return schemas;
+        }
+
+        private async Task<string> BuildQuery(List<string> schemas, string userRequest)
+        {
+            return string.Empty;
+        }
+
+        private bool ValidateQuery(string query)
+        {
+            return false;
+        }
+
+        private async Task<string> RunQuery(string query)
+        {
+            var reply = await _grpcClient.ExecuteQueryAsync(
+                                   new QueryRequest { InstanceName = _instanceName, DbName = _dbName, Query = "SELECT * FROM sys.tables" });
+
+            return reply.QueryResults;
+        }
+
+        private IList<ChatMessage> GetDefaultChatInstructions()
+        {
+            var combinedInstructions = $@"You are an expert in SQL";
+
+            Console.WriteLine(combinedInstructions);
+
+            return new List<ChatMessage>
+            {
+                new ChatMessage(ChatRole.System, combinedInstructions),
+            };
+        }
+    }
+}
